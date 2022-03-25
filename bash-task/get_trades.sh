9782fcb06334cdca9b0c9628dafac0d5d5b084ff
dav-cho <chodavid@gmail.com>
2022-03-25 10:16:26 -0400

    cleanup

--- bash-task/get_trades.sh
+++ bash-task/get_trades.sh
@@ -5,34 +5,33 @@ get_timeseries_market_trades() {
   local MARKETS=$1
   local TIME_OFFSET=$2
   local PAGE_SIZE=10000
-
   local START_TIME=$(date -u -v "-${TIME_OFFSET}S" +'%Y-%m-%dT%H:%M:%SZ')
-  local URL="${BASE_URL}?markets=${MARKETS}&start_time=${START_TIME}&page_size=${PAGE_SIZE}&pretty=true"
 
-  local RESPONSE=$(curl $URL)
-  echo $RESPONSE
+  local url="${BASE_URL}?markets=${MARKETS}&start_time=${START_TIME}&page_size=${PAGE_SIZE}&pretty=true"
+  local response=$(curl $url)
+  echo $response
 
-  local PAGE_COUNT=1
-  echo $RESPONSE > "page-${PAGE_COUNT}.json"
-  PAGE_COUNT=$((PAGE_COUNT+1))
+  local page_count=1
+  echo $response > "page-${page_count}.json"
+  page_count=$((page_count+1))
 
   # The following was added in case the results are greater than the page size
-  local NEXT_URL=$(echo $RESPONSE | python -c 'import sys, json; print(json.load(sys.stdin).get("next_page_url", ""))')
-  echo '~' $(echo $RESPONSE | python -c 'import sys, json; print(len(json.load(sys.stdin).get("data", [])))') 'Trades found in page.'
+  local next_url=$(echo $response | python -c 'import sys, json; print(json.load(sys.stdin).get("next_page_url", ""))')
+  echo '~' $(echo $response | python -c 'import sys, json; print(len(json.load(sys.stdin).get("data", [])))') 'Trades found in page.'
 
-  while [ $#NEXT_URL -gt 0 ]
+  while [ $#next_url -gt 0 ]
   do
-    local NEXT_RESPONSE=$(curl "$NEXT_URL")
-    echo $NEXT_RESPONSE
-    echo $NEXT_RESPONSE > "page-${PAGE_COUNT}.json"
-    PAGE_COUNT=$((PAGE_COUNT+1))
+    local next_response=$(curl "$next_url")
+    echo $next_response
+    echo $next_response > "page-${page_count}.Json"
+    page_count=$((page_count+1))
 
-    NEXT_URL=$(echo $NEXT_RESPONSE | python -c 'import sys, json; print(json.load(sys.stdin).get("next_page_url", ""))')
+    next_url=$(echo $next_response | python -c 'import sys, json; print(json.load(sys.stdin).get("next_page_url", ""))')
 
-    echo '~' $(echo $NEXT_RESPONSE | python -c 'import sys, json; print(len(json.load(sys.stdin).get("data", [])))') 'Trades found in page.'
+    echo '~' $(echo $next_response | python -c 'import sys, json; print(len(json.load(sys.stdin).get("data", [])))') 'Trades found in page.'
   done
 
-  echo '~' $((PAGE_COUNT-1)) 'Pages Found.'
+  echo '~' $((page_count-1)) 'Pages Found.'
 }
 
 get_timeseries_market_trades $1 $2
